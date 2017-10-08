64dcd9e956f31c6f5bbe02904504119a2ae7c313
Asger <asger@matchblog.dk>
2017-10-08 17:36:52 +0200

    Første Ændringer
    Virker ikke...

--- SuperPrutter/ViewController.swift
+++ SuperPrutter/ViewController.swift
@@ -7,19 +7,74 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    let prutter = ["prut1", "prut2"]
+    
+    @IBOutlet weak var prutteKnap: UIButton!
+    @IBOutlet weak var pruttePicker: UIPickerView!
+    @IBOutlet weak var prutteLabel: UILabel!
+    
+    // Prutter Sound Player
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+ /*
+    // Prutter Sound Player
+        let path = Bundle.main.url(forResource: "fart", withExtension: "wav")
+        let soundURL = URL(fileURLWithPath: path!)
+        
+        do {
+            try prutteLyd = AVAudioPlayer(contentsOf: soundURL)
+            prutteLyd.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+*/
+        
+        pruttePicker.dataSource = self
+        pruttePicker.delegate = self
+        
+    }
+    
+    //MARK:- PLAY SOUND
+    func playSound() {
+        let url = Bundle.main.url(forResource: "btn", withExtension: "wav")!
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            guard let player = player else { return }
+            
+            player.prepareToPlay()
+            player.play()
+        } catch let error as NSError {
+            print(error.description)
+        }
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return prutter.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return prutter[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        prutteLabel.text = prutter[row]
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func prutteKnapTrykket(_ sender: Any) {
+        playSound()
     }
-
-
+    
 }
 
