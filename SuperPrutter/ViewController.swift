00440c0af5be4095d7b69240cc96d85ea661ce8d
Asger <asger@matchblog.dk>
2017-10-10 20:25:05 +0200

    TableVIew

--- SuperPrutter/ViewController.swift
+++ SuperPrutter/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
