c15e6fe58413781f13eb874ee26d251c491262e1
Lambrechto <danielhlambrecht@gmail.com>
2017-10-10 18:38:53 +0200

    slettet storyboard og picker relateret kode

--- SuperPrutter/ViewController.swift
+++ SuperPrutter/ViewController.swift
@@ -9,15 +9,13 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class ViewController: UIViewController {
 
     let prutter = [fart1,fart2]
     
     var selectedFart: Fart = fart1
     
-    @IBOutlet weak var prutteKnap: UIButton!
-    @IBOutlet weak var pruttePicker: UIPickerView!
-    @IBOutlet weak var prutteLabel: UILabel!
+
     
     // Prutter Sound Player
     
@@ -26,21 +24,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     override func viewDidLoad() {
         super.viewDidLoad()
         
- /*
-    // Prutter Sound Player
-        let path = Bundle.main.url(forResource: "fart", withExtension: "wav")
-        let soundURL = URL(fileURLWithPath: path!)
         
-        do {
-            try prutteLyd = AVAudioPlayer(contentsOf: soundURL)
-            prutteLyd.prepareToPlay()
-        } catch let err as NSError {
-            print(err.debugDescription)
-        }
-*/
-        
-        pruttePicker.dataSource = self
-        pruttePicker.delegate = self
         
     }
     
@@ -60,23 +44,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         }
     }
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return prutter.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return prutter[row].name
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        prutteLabel.text = prutter[row].name
-        selectedFart = prutter[row]
-    }
-
     @IBAction func prutteKnapTrykket(_ sender: Any) {
         playSound()
     }
